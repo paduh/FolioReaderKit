debdf972ecded6aa828d392450d0512d7ae12be0
Perfect Aduh <paduh@getcarcon.co>
2020-05-28 00:00:34 +0100

    undo scrolling

--- Source/FolioReaderCenter.swift
+++ Source/FolioReaderCenter.swift
@@ -155,7 +155,7 @@ open class FolioReaderCenter: UIViewController, UICollectionViewDelegate, UIColl
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.backgroundColor = background
         collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
-        enableScrollBetweenChapters(scrollEnabled: false)
+        enableScrollBetweenChapters(scrollEnabled: true)
         view.addSubview(collectionView)
         
         if #available(iOS 11.0, *) {
